bf7a1020e1d6e34b7427a79a88353cd047d226f1
tlitookilakin <swyrlyy@gmail.com>
2021-12-02 19:30:02 -0500

    Add project files.

---
+++ MUMPs/Events.cs
@@ -0,0 +1,24 @@
+﻿using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUMPs
+{
+    class Events
+    {
+        public static void DayStarted(object sender, DayStartedEventArgs ev)
+        {
+            foreach(GameLocation loc in Game1.locations)
+            {
+                if (loc.Name != "Woods")
+                {
+                    Props.Stumps.SpawnMapStumps(loc);
+                }
+            }
+        }
+    }
+}
---
+++ MUMPs/Props/Stumps.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+
+namespace MUMPs.Props
+{
+    class Stumps
+    {
+        internal static void SpawnMapStumps(GameLocation location)
+        {
+            string prop = location.getMapProperty("Stumps");
+            if (prop == null) { return; }
+            string[] stumpList = prop.Split(' ');
+            for(int i = 0; i+2 < stumpList.Length; i += 3)
+            {
+                //x, y, unused
+
+                Point pos = new Point(int.Parse(stumpList[i]), int.Parse(stumpList[i + 1]));
+                if (location.isAreaClear(new Rectangle(pos, new Point(2, 2))))
+                {
+                    location.addResourceClumpAndRemoveUnderlyingTerrain(600, 2, 2, pos.ToVector2());
+                }
+            }
+        }
+    }
+}
