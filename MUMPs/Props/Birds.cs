6e62b2844f05b964fbc9eb6944760c08f36eb948
tlitookilakin <swyrlyy@gmail.com>
2022-01-03 17:11:28 -0500

    change birds to use tile props

--- MUMPs/Props/Birds.cs
+++ MUMPs/Props/Birds.cs
@@ -5,6 +5,9 @@ using StardewValley;
 using StardewValley.BellsAndWhistles;
 using System;
 using System.Collections.Generic;
+using xTile;
+using xTile.Layers;
+using xTile.Tiles;
 
 namespace MUMPs.Props
 {
@@ -12,40 +15,39 @@ namespace MUMPs.Props
     {
         private static PerchingBirds birds = null;
         private static Vector2 origin = new(8f, 14f);
+        private static readonly Dictionary<GameLocation, (Point[] perch, Point[] roost)> spots = new();
 
         public static void EnterLocation(GameLocation location)
         {
 
             birds = null;
 
-            string[] data = Utils.MapPropertyArray(location, "Birds");
-            if(data.Length < 1) { return; }
-
-            List<Point> perches = new();
-            Point empty = new(0, 0);
-
-            for(int i = 1; i + 1 < data.Length; i += 2)
+            if (!int.TryParse(location.getMapProperty("Birds"), out int count))
             {
-                Point perch = Utils.StringsToPoint(data[i], data[i + 1]);
-                if (perch != empty)
-                {
-                    perches.Add(perch);
-                }
+                ModEntry.monitor.Log("Invalid value for 'Birds' property; could not convert to number.");
+                return;
             }
 
-            birds = new PerchingBirds(Game1.birdsSpriteSheet, 2, 16, 16, origin, perches.ToArray(), perches.ToArray())
+            var spots = GetSpotsFor(location);
+            count = Math.Clamp(count, 0, Math.Min(spots.perch.Length, spots.roost.Length));
+
+            birds = new PerchingBirds(Game1.birdsSpriteSheet, 2, 16, 16, origin, spots.perch, spots.roost)
             {
                 roosting = (Game1.timeOfDay >= 1800)
             };
-            int count = Math.Clamp(int.Parse(data[0]), 0, perches.Count - 1);
             for(int i = 0; i < count; i++)
             {
                 birds.AddBird((Game1.currentSeason == "fall") ? 10 : Game1.random.Next(0, 4));
             }
         }
+        public static void StartDay()
+        {
+            spots.Clear();
+        }
         public static void Cleanup()
         {
             birds = null;
+            spots.Clear();
         }
         public static void DrawAbove(SpriteBatch batch)
         {
@@ -56,6 +58,50 @@ namespace MUMPs.Props
         {
             if(birds == null) { return; }
             birds.Update(time);
+        }
+        public static (Point[] perch, Point[] roost) GetSpotsFor(GameLocation location)
+        {
+
+            if (location == null)
+                return (Array.Empty<Point>(), Array.Empty<Point>());
+
+            if (!spots.TryGetValue(location, out var spot)){
+                spot = ScanForSpots(location.Map);
+                spots.Add(location, spot);
+            }
+
+            return spot;
+        }
+        public static (Point[] perch, Point[] roost) ScanForSpots(Map map)
+        {
+            if (map == null)
+                return (Array.Empty<Point>(), Array.Empty<Point>());
+
+            Layer paths = map.GetLayer("Paths");
+            if (paths == null)
+                return (Array.Empty<Point>(), Array.Empty<Point>());
+
+            List<Point> perches = new();
+            List<Point> roosts = new();
+
+            int w = paths.LayerWidth;
+            int h = paths.LayerHeight;
+            for (int x = 0; x < w; x++)
+            {
+                for(int y = 0; y < h; y++)
+                {
+                    Tile tile = paths.Tiles[new(x, y)];
+                    if (tile != null)
+                    {
+                        if (tile.Properties.ContainsKey("perch"))
+                            perches.Add(new(x, y));
+                        if (tile.Properties.ContainsKey("roost"))
+                            roosts.Add(new(x, y));
+                    }
+                }
+            }
+
+            return (perches.ToArray(), roosts.ToArray());
         }
     }
 }
