62c047e56e8b73c9551e3a3b87b3629c5af5487a
tlitookilakin <swyrlyy@gmail.com>
2022-01-05 22:23:37 -0500

    Add Butterfly Property

---
+++ MUMPs/Props/Butterflies.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+namespace MUMPs.Props
+{
+    [HarmonyPatch(typeof(GameLocation),"resetLocalState")]
+    class Butterflies
+    {
+        public static void Postfix(GameLocation __instance)
+        {
+            EnterLocation(__instance);
+        }
+        public static void EnterLocation(GameLocation location)
+        {
+            if (!int.TryParse(location.getMapProperty("Butterflies"), out int count))
+                return;
+
+            bool isIsland = location.getMapProperty("LocationContext") == "Island" || location.Name.StartsWith("Island");
+
+            location.instantiateCrittersList();
+            for(int i = 0; i < count; i++)
+            {
+                location.addCritter(new Butterfly(location.getRandomTile(), isIsland).setStayInbounds(true));
+            }
+        }
+    }
+}
