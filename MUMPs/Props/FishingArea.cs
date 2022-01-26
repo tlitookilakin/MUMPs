4efb0c56a8f9ccdfe8363b940f74602a1cb2cc29
tlitookilakin <swyrlyy@gmail.com>
2022-01-26 12:49:41 -0500

    fix order of fishingregion to be more consistent

--- MUMPs/Props/FishingArea.cs
+++ MUMPs/Props/FishingArea.cs
@@ -14,12 +14,12 @@ namespace MUMPs.Props
             string[] data = Utils.MapPropertyArray(__instance, "FishingRegions");
             for(int i = 0; i + 4 < data.Length; i += 5)
             {
-                if( !int.TryParse(data[i], out int ind))
+                if( !int.TryParse(data[i + 4], out int ind))
                 {
                     return false;
                 }
-                Vector2 origin = Utils.StringsToVec2(data[i + 1], data[i + 2]);
-                Vector2 size = Utils.StringsToVec2(data[i + 3], data[i + 4]);
+                Vector2 origin = Utils.StringsToVec2(data[i], data[i + 1]);
+                Vector2 size = Utils.StringsToVec2(data[i + 2], data[i + 3]);
                 if (tile.X >= origin.X && tile.Y >= origin.Y && tile.X < origin.X + size.X && tile.Y < origin.Y + size.Y)
                 {
                     __result = ind;
