2569f2842851db083b8434b2e30dea0b6eb94e9d
tlitookilakin <swyrlyy@gmail.com>
2022-01-27 21:32:17 -0500

    Revert "streamlined pytk compat"
    This reverts commit ad99122d41a5610a93b36ec8b9da44abae7e862f.

--- MUMPs/Props/LightingLayer.cs
+++ MUMPs/Props/LightingLayer.cs
@@ -20,9 +20,13 @@ namespace MUMPs.Props
         private static Dictionary<TileSheet, Texture2D> sheets;
         public static void Reload()
         {
-            string classname = (ModEntry.helper.ModRegistry.IsLoaded("Platonymous.Toolkit") ? "PyDisplayDevice" : "SXnaDisplayDevice";
-            sheets = (Dictionary<TileSheet, Texture2D>)AccessTools.TypeByName("PyDisplayDevice").
+            if (ModEntry.helper.ModRegistry.IsLoaded("Platonymous.Toolkit"))
+                sheets = (Dictionary<TileSheet, Texture2D>)AccessTools.TypeByName("PyDisplayDevice").
                     GetField("m_tileSheetTextures2", BindingFlags.NonPublic | BindingFlags.Instance).
+                    GetValue(Game1.mapDisplayDevice);
+            else
+                sheets = (Dictionary<TileSheet, Texture2D>)AccessTools.TypeByName("SXnaDisplayDevice").
+                    GetField("m_tileSheetTextures", BindingFlags.NonPublic | BindingFlags.Instance).
                     GetValue(Game1.mapDisplayDevice);
         }
         public static void Draw(float intensity)
