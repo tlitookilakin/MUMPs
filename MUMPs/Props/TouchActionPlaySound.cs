37380cd36fae92d415d252010d4fa8f348144cdb
tlitookilakin <swyrlyy@gmail.com>
2022-07-20 22:32:47 -0400

    added playsound touchaction

---
+++ MUMPs/Props/TouchActionPlaySound.cs
@@ -0,0 +1,28 @@
+﻿using AeroCore;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+
+namespace MUMPs.Props
+{
+    [ModInit]
+    internal class TouchActionPlaySound
+    {
+        internal static void Init()
+        {
+            ModEntry.AeroAPI.RegisterTouchAction("PlaySound", HandleTouchAction);
+        }
+        private static void HandleTouchAction(Farmer who, string what, Point tile, GameLocation where)
+        {
+            try
+            {
+                where.playSoundAt(what.Trim(), tile.ToVector2());
+            } catch (Exception e)
+            {
+                ModEntry.monitor.Log($"Could not play sound cue '{what.Trim()}'.", LogLevel.Warn);
+                ModEntry.monitor.Log(e.Message);
+            }
+        }
+    }
+}
