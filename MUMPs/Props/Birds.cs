109226c3da02b7f4996d1956f79b5cbf4486e6ea
tlitookilakin <swyrlyy@gmail.com>
2022-09-20 18:28:14 -0400

    add bird variant support

--- MUMPs/Props/Birds.cs
+++ MUMPs/Props/Birds.cs
@@ -8,6 +8,7 @@ using StardewValley;
 using StardewValley.BellsAndWhistles;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using xTile;
 using xTile.Layers;
 using xTile.Tiles;
@@ -30,15 +31,26 @@ namespace MUMPs.Props
         private static void EnterLocation(GameLocation location)
         {
             birds.Value = null;
-            string prop = location.getMapProperty("Birds");
-            if (prop.Length == 0)
+            var prop = location.getMapProperty("Birds").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (prop.Length < 1)
                 return;
 
-            if (!int.TryParse(prop, out int count))
+            if (!int.TryParse(prop[0], out int count))
             {
                 ModEntry.monitor.Log("Invalid value for 'Birds' property; could not convert to number.", LogLevel.Warn);
                 return;
             }
+
+            List<int> types = new();
+            if (prop.Length < 2)
+                if (Game1.currentSeason == "fall")
+                    types.Add(10);
+                else
+                    types.AddRange(new[] {0, 1, 2, 3});
+            else
+                for (int i = 1; i < prop.Length; i++)
+                    if (int.TryParse(prop[i], out int ind))
+                        types.Add(ind);
 
             var (perch, roost) = GetSpotsFor(location);
             count = Math.Clamp(count, 0, Math.Min(perch.Length, roost.Length));
@@ -49,7 +61,7 @@ namespace MUMPs.Props
             };
 
             for(int i = 0; i < count; i++)
-                birds.Value.AddBird((Game1.currentSeason == "fall") ? 10 : Game1.random.Next(0, 4));
+                birds.Value.AddBird(Game1.random.Next(types));
         }
         private static void Cleanup() => birds.ResetAllScreens();
         private static void DrawAbove(SpriteBatch batch) => birds.Value?.Draw(batch);
