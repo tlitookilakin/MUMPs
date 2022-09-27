d2688ec2d0b63bc8b80ca803c5afade4156f0e91
tlitookilakin <swyrlyy@gmail.com>
2022-09-27 13:08:20 -0400

    add quickwarp

---
+++ MUMPs/Props/TouchActionQuickWarp.cs
@@ -0,0 +1,24 @@
+﻿using AeroCore;
+using AeroCore.Utils;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using System;
+
+namespace MUMPs.Props
+{
+	[ModInit]
+	internal class TouchActionQuickWarp
+	{
+		internal static void Init()
+		{
+			ModEntry.AeroAPI.RegisterTouchAction("QuickWarp", HandleWarp);
+		}
+		private static void HandleWarp(Farmer who, string action, Point tile, GameLocation where)
+		{
+			var split = action.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			if (split.Length < 3 || !int.TryParse(split[1], out int x) || !int.TryParse(split[2], out int y))
+				return;
+			Maps.QuickWarp(split[0], x, y, false);
+		}
+	}
+}
